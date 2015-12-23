78186d830c316e3d36dd23f041b5cf80a608bfe9
Parikshit Machwe <pmachwe@gmail.com>
2015-12-23 23:27:56 +0530

    Added a separate custom-file for emacs.

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
 
-;;; Set up package
+;; Set up package
 
 (require 'package)
 (setq package-archives '(("org"       . "http://orgmode.org/elpa/")
@@ -18,7 +18,7 @@
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
 
-;;; Bootstrap use-package
+;; Bootstrap use-package
 ;; Install use-package if it's not already installed.
 ;; use-package is used to configure the rest of the packages.
 (unless (package-installed-p 'use-package)
@@ -31,29 +31,5 @@
 (require 'bind-key)
 ;(setq use-package-verbose t)
 
-;;; Load the config
+;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("0fb6369323495c40b31820ec59167ac4c40773c3b952c264dd8651a3b704f6b5" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(package-selected-packages
-   (quote
-    (org-tree-slide powerline htmlize color-theme-sanityinc-tomorrow elpy smartparens yasnippet company ace-window avy expand-region multiple-cursors helm-gtags helm-swoop helm flx-ido org-present org-bullets use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-block-begin-line ((t (:inherit org-meta-line :background "gray10"))))
- '(org-document-title ((t (:underline t :weight bold :height 1.3))))
- '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.15))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.05)))))
