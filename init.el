762d15c58c2ed1c12bd81328de8f53f60b840213
Parikshit Machwe <pmachwe@gmail.com>
2016-07-05 22:29:28 +0530

    Removing org-mode and marmalade repos.

--- init.el
+++ init.el
@@ -12,10 +12,8 @@
 ;; Set up package
 
 (require 'package)
-(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
-                         ("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("melpa"     . "http://melpa.milkbox.net/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 ;; Bootstrap use-package
