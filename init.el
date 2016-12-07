39949eff6fce74c017ef67d8586840360e87f4d2
pmachwe <pmachwe@gmail.com>
2016-12-07 22:12:35 +0530

    Load el when possible

--- init.el
+++ init.el
@@ -25,26 +25,40 @@
 
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)                ;; if you use :diminish
+(require 'diminish)
 (require 'bind-key)
-;(setq use-package-verbose t)
 
-;; Load the config
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+;; Taken from comments on this post;
+;; http://endlessparentheses.com/init-org-Without-org-mode.html
+(defun my/load-el-before-org (org-file el-file)
+  "If ORG-FILE is not recently changed, load the EL-FILE."
+  (if (file-exists-p org-file)
+      (if (and (file-exists-p el-file)
+               (time-less-p (nth 5 (file-attributes org-file))
+                            (nth 5 (file-attributes el-file))))
+          (load-file el-file)
+        (if (fboundp 'org-babel-load-file)
+            (org-babel-load-file org-file)
+          (message "Function not found: org-babel-load-file")
+          (load-file el-file)))
+    (error "Init org file '%s' missing" org-file)))
 
-;; Load utility functions
-(org-babel-load-file (concat user-emacs-directory "functions.org"))
+(my/load-el-before-org (concat user-emacs-directory "config.org")
+                       (concat user-emacs-directory "config.el"))
+
+(my/load-el-before-org (concat user-emacs-directory "functions.org")
+                       (concat user-emacs-directory "functions.el"))
 
 ;; Load the work-specific file which cannot be uploaded to Github
-(defvar work-el "~/.emacs.d/work.el"
+(defvar work-el (concat user-emacs-directory "work.el")
   "Work specific settings.")
 
-(defvar work-org "~/.emacs.d/work.org"
+(defvar work-org (concat user-emacs-directory "work.org")
   "Work specific settings in 'org-mode'.")
 
 ;; Load org if available or else try el
 (if (file-exists-p work-org)
-    (org-babel-load-file work-org)
+    (my/load-el-before-org work-org work-el)
   (if (file-exists-p work-el)
       (load-file work-el)))
 
