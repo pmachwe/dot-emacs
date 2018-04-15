a1bf9f0e257b2facd2a1ba0e417103ae011f94d7
Parikshit Machwe <pmachwe@Parikshits-MacBook-Air.local>
2018-04-15 16:37:09 +0530

    Cleaned-up org-mode setup.

--- init.el
+++ init.el
@@ -13,7 +13,8 @@
 
 (require 'package)
 (setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("melpa"     . "http://melpa.org/packages/")))
+                         ("melpa"     . "http://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
 ;; Bootstrap use-package
